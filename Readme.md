83f3857a8460a9d02d10c5066bc1d2e5d90b549e
Guevaer Arnaud <guevaer8@gmail.com>
2025-11-10 16:32:17 +0100

    push repo

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Basic Concepts: Console, Terminal, Shell, and Commands
+
+This document explains the fundamental concepts of interacting with a computer using the command line.
+
+## 1. Console
+The **console** is the general concept of **interacting with a computer using text**. Originally, it was a physical device consisting of a screen and a keyboard directly connected to the machine. Today, the console is often virtual, but it still represents the idea of a text-based interface that allows communication with the system.
+
+---
+
+## 2. Terminal
+A **terminal** is a program or application that opens a window for typing commands and viewing results. The terminal does not execute commands itself; it receives input, sends it to the shell, and displays the output returned by the shell. It essentially manages text-based input and output between the user and the shell.
+
+---
+
+## 3. Shell
+The **shell** is the program that interprets and executes commands. It receives commands from the terminal, analyzes them, sends instructions to the system to perform actions or calculations, and then returns the results to the terminal for display.
+
+---
+
+## 4. Command and Arguments
+A **command** is an instruction given to the shell, and **arguments** are additional options or parameters that modify the behavior of the command or specify its target. When a command is typed into the terminal, the terminal sends it to the shell, which interprets and executes it via the system, then returns the result to the terminal for display.
+
+---
+
+## Conclusion
+The console represents the concept of text-based interaction, the terminal is the window that manages input and output, the shell is the program that interprets and executes commands, and commands with arguments are the instructions provided by the user. Together, these elements form a complete system that allows users to communicate with and control the computer using text commands.
---
+++ run.sh
@@ -0,0 +1,79 @@
+chmod +x run.sh
+
+#-----------
+
+# VARIABLES
+DATE=$(date +"%Y-%m-%dT%H:%M:%S.%3N%z")
+PATH_TO_FILE=$(realpath "$0")
+TMP_DIR="$(pwd)/tmp"
+
+# COLORS
+BLUE_UNDERLINE="\033[34;4m"
+GREEN="\033[32m"
+RESET="\033[0m"
+
+#-----------
+
+#ARGUMENTS
+URL_FILE="$1"
+DOWNLOAD_DIR="$2"
+ARCHIVES_DIR="$3"
+
+HEADERS_FILE="$2/headers.txt"
+ARCHIVE_NAME="D$(date +"%Y-%m-%dT%H-%M-%S").tar.gz"
+
+#-----------
+
+# FUNCTIONS
+download_url() {
+    for URL in $(cat "$URL_FILE" | tr -d '\r'); do
+        FILE_NAME=$(basename "$URL")
+        echo -e "> Downloading '${BLUE_UNDERLINE}${URL}${RESET}'…"
+        curl -s -k -L -D "${TMP_DIR}/${FILE_NAME}.headers" -o "${TMP_DIR}/${FILE_NAME}" "$URL"
+        if [[ -s "${TMP_DIR}/${FILE_NAME}" ]]; then
+            echo -e "  ${GREEN}Done${RESET}"
+        else
+            echo -e "  Download failed for ${URL}${RESET}"
+        fi
+    done
+}
+
+copy_json() {
+   cp "$TMP_DIR"/*.json "$DOWNLOAD_DIR"/
+   cp "$TMP_DIR"/*.headers "$DOWNLOAD_DIR"/
+   echo -e "${GREEN}Done${RESET}"
+}
+
+headers_compile() {
+   > "$HEADERS_FILE"
+   for header in "$DOWNLOAD_DIR"/*.headers; do
+      echo "### $(basename "$header"):" >> "$HEADERS_FILE"
+      cat "$header" >> "$HEADERS_FILE"
+      echo "" >> "$HEADERS_FILE"
+   done
+   echo -e "  ${GREEN}Done${RESET}"
+}
+
+compressing_files() {
+   tar -czf "$ARCHIVES_DIR/$ARCHIVE_NAME" -C "$DOWNLOAD_DIR" .
+   echo -e "${GREEN}Done${RESET} (archive file name: $ARCHIVE_NAME)"
+}
+
+#-----------
+
+# SCRIPT EXECUTION
+echo "> Bash script starting at: $DATE"
+echo "Script full path: '$PATH_TO_FILE'"
+mkdir -p "$TMP_DIR"
+echo "$TMP_DIR file created"
+download_url
+echo "> Copying JSON files from '$TMP_DIR' to '$2'…"
+mkdir -p "$2"
+copy_json
+echo "> Compiling HTTP response headers from '$TMP_DIR' to '$2'…"
+headers_compile
+echo "> Compressing all files in '$2' to '$3'…"
+mkdir -p "$3"
+compressing_files
+echo "> Bash script ending at: $DATE"
+echo "Bye!"
